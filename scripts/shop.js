1af3c8559663c50e3e35d837c4ccacfc125d7f8a
mennaafathy1014 <menna10awaden@gmail.com>
2022-12-21 08:52:55 +0200

    deleted

--- scripts/shop.js
+++
@@ -1,8 +0,0 @@
-const imgHandler = document.querySelector('.product-previews');
-const productPreview = document.querySelector('.main-preview');
-
-console.log(imgHandler);
-
-imgHandler.addEventListener('click', (e) => {
-    productPreview.src = e.target.src;
-})
